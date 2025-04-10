9339c49275e250186a5028f513c44c8570a87b88
MYI26 <ymimoun@g.jct.ac.il>
2025-04-10 15:00:27 +0200

    My first push"

---
+++ src/components/DisplayArea.jsx
@@ -0,0 +1,11 @@
+function DisplayArea() {
+    return (
+      <div className="display-area">
+        <h2>Display Area</h2>
+        <p>The entered text will be shown here.</p>
+      </div>
+    );
+  }
+  
+  export default DisplayArea;
+  
---
+++ src/components/EditorArea.jsx
@@ -0,0 +1,11 @@
+function EditorArea() {
+    return (
+      <div className="editor-area">
+        <h2>Editing Area</h2>
+        <textarea className="thin-textarea" placeholder="Type your text here..." />
+      </div>
+    );
+  }
+  
+  export default EditorArea;
+  
